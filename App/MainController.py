7dcf45a9af8d1a9b6f8f7246c10db8360a984248
Artem Pushkar <artem.p@droam.com>
2024-05-06 02:35:52 +0300

    Added rabbitmq broker

--- App/MainController.py
+++ App/MainController.py
@@ -1,21 +1,25 @@
 import flask
 from app import song_recommender
 from flask import request
+import rabbitMqController
 
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/api/v1/music', methods=['GET'])
-def api_all():
-
-    song_id_first = request.args['song_id_first']
-    song_id_second = request.args['song_id_second']
-
-    recommendations = song_recommender(song_id_first, song_id_second)
-
-    return recommendations.to_json(orient='records')
+rabbitMqController.consume()
 
 
-app.run()
+# @app.route('/api/v1/music', methods=['GET'])
+# def api_all():
+#
+#     song_id_first = request.args['song_id_first']
+#     song_id_second = request.args['song_id_second']
+#
+#     recommendations = song_recommender(song_id_first, song_id_second)
+#
+#     return recommendations.to_json(orient='records')
+#
+#
+# app.run()
